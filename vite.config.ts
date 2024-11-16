bb37d231d6ebf40a30e25457bff5ef600e4fb9e9
Wesley Oliveira <wesleydrum10@gmail.com>
2024-11-16 14:38:32 -0300

    docs: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Tic Tac Toe',
+        short_name: 'Jogo da velha',
+        description: 'Instalador do jogo tic tac toe',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icons/X.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/X.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
